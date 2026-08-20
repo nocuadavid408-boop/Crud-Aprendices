d1828fba47dea39015183e753e00987b36e0e5f6
jhonalexbeltran2008 <nocuadavid408@gmail.com>
2026-08-20 14:19:37 -0500

    Servidor  funcional

--- app.js
+++ app.js
@@ -3,12 +3,34 @@ require('dotenv/config');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+//libreria para leer archivos
+const sistemaArchivos = require ('fs');
+const ruta = require('path')
+//generar una ruta para el archivo aprendices.json
+const rutaArchivojson = ruta.join(__dirname,'listaDatos.json');
 
+//------------------------
+app.use(express.json());
+//------------------------
+
+//ruta raiz
 app.get('/', (req, res) => {
   res.send('Servidor inicializado correctamente');
 });
 
+//endpoint para obtener todos los aprendices
+app.get('/api/aprendices', (req, res) => {
+  sistemaArchivos.readFile(rutaArchivojson, "utf-8", (error, datos) =>{
+    if (error){
+      res.status(500).json({error: "Error al leer el archivo, conexion db"})
+    }
+    const listaaprendices = JSON.parse(datos);
+    res.json(listaaprendices)
+  })
+});
+
+
+//mode de escucha del servidor
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);
 });
